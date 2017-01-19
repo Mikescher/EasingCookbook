797b762eb5e11bb85dbfb5933bd33acb97c7b3dc
Mike Schwörer <mailport@mikescher.de>
2017-01-19 21:04:13 +0100

    working graph

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,44 @@
-var ctx = document.getElementById("previewChart");
 
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero:true
+Chart.defaults.global.animation.duration = 0;
+
+new Chart(document.getElementById("previewChart"), {
+    type: 'line',
+    data: {
+        cubicInterpolationMode: 'monotone',
+        datasets: createDataSet("t < .5 ? 8 * t * t * t * t : 1 - 8 * (t - 1) * (t - 1) * (t - 1) * (t - 1)")
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom'
+            }]
+        },
+        animation: {
+            animation : false,
+            animateScale: false,
+            animateRotate: false,
         }
-      }]
     }
-  }
 });
+
+function createDataSet(func) {
+    const RESOLUTION = 1000;
+
+    let compiled = eval('(function (t){return ' + func + ';})')
+
+    let data = [];
+
+    for (let i=0; i < RESOLUTION; i++) {
+        data.push({x: i/RESOLUTION, y: compiled(i/RESOLUTION)});
+    }
+
+    return [{
+        label: 'return '+func,
+        data: data,
+        fill: false,
+        borderColor: 'rgba(0, 0, 0, 0.75)',
+        pointRadius: 0,
+        borderWidth: 4,
+    }];
+}
